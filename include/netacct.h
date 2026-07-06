1e5187534d1b5083778ac484a06ae36c2669280f
Mohamed-Ismail Mejri <imejri@hotmail.com>
2026-07-06 20:19:47 +0200

    Add IPv6-capable address structures

--- include/netacct.h
+++ include/netacct.h
@@ -11,6 +11,10 @@
 #define MAX_SUBNET_TEXT 64
 #define MAX_IP_ENTRIES 4096
 #define IP_HASH_BUCKETS 1024
+#define NETACCT_ADDR_BYTES 16
+
+#define NETACCT_IPV4 4
+#define NETACCT_IPV6 6
 
 #define NETACCT_DEFAULT_IFACE "eth0"
 #define NETACCT_DEFAULT_ROOT "/var/lib/netacct"
@@ -21,14 +25,19 @@
 
 extern volatile sig_atomic_t netacct_running;
 
+struct netacct_addr {
+    uint8_t ipv;
+    uint8_t bytes[NETACCT_ADDR_BYTES];
+};
+
 struct ip_record {
-    uint32_t ip;
+    struct netacct_addr addr;
     uint64_t rx;
     uint64_t tx;
 };
 
 struct ip_counter {
-    uint32_t ip;
+    struct netacct_addr addr;
     uint64_t rx_bytes;
     uint64_t tx_bytes;
     struct ip_counter *next;
@@ -61,11 +70,16 @@ struct cfg {
     int has_local_net;
     char subnet_text[MAX_SUBNET_TEXT];
 
+    uint8_t local6_net[NETACCT_ADDR_BYTES];
+    int local6_prefix;
+    int has_local6_net;
+    char subnet6_text[MAX_SUBNET_TEXT];
+
     int pcap_buffer_mb;
     int top_n;
 };
 
-struct __attribute__((packed)) ip_entry_on_disk {
+struct __attribute__((packed)) ip_entry_v4_on_disk {
     uint8_t ipv;
     uint8_t pad;
     uint32_t addr;
@@ -73,9 +87,18 @@ struct __attribute__((packed)) ip_entry_on_disk {
     uint64_t tx_delta;
 };
 
+struct __attribute__((packed)) ip_entry_v6_on_disk {
+    uint8_t ipv;
+    uint8_t pad;
+    uint8_t addr[NETACCT_ADDR_BYTES];
+    uint64_t rx_delta;
+    uint64_t tx_delta;
+};
+
 struct pcap_runtime_stats {
     uint64_t packets_seen;
     uint64_t ipv4_packets;
+    uint64_t ipv6_packets;
     uint64_t local_packets;
     uint64_t accounted_bytes;
     unsigned int pcap_recv;
@@ -87,10 +110,15 @@ void cfg_set_defaults(struct cfg *cfg);
 int cfg_load_file(struct cfg *cfg, const char *path);
 int cfg_apply_option(struct cfg *cfg, const char *key, const char *value);
 int parse_ipv4_cidr(const char *cidr, uint32_t *out_net, uint32_t *out_mask);
+int parse_ipv6_cidr(const char *cidr, uint8_t out_net[NETACCT_ADDR_BYTES], int *out_prefix);
 int detect_iface_ipv4_network(const char *iface, uint32_t *out_net, uint32_t *out_mask,
                               char *out_text, size_t out_text_len);
+int detect_iface_ipv6_network(const char *iface, uint8_t out_net[NETACCT_ADDR_BYTES],
+                              int *out_prefix, char *out_text, size_t out_text_len);
 int cfg_ip_is_local(const struct cfg *cfg, uint32_t ip_be);
+int cfg_ip6_is_local(const struct cfg *cfg, const uint8_t ip6[NETACCT_ADDR_BYTES]);
 const char *format_ipv4_host(uint32_t ip_host, char *buf, size_t len);
+const char *format_netacct_addr(const struct netacct_addr *addr, char *buf, size_t len);
 
 int collector_init(struct cfg *cfg);
 int collector_run(struct cfg *cfg);
@@ -104,10 +132,10 @@ void pcap_get_runtime_stats(struct pcap_runtime_stats *out);
 void *poller_thread_fn(void *arg);
 int poller_persist_last_counts(const struct cfg *cfg);
 
-int ipacct_add_client(uint32_t ip);
-int ipacct_del_client(uint32_t ip);
-int ipacct_update_rx(const char *iface, uint32_t ip, uint32_t bytes);
-int ipacct_update_tx(const char *iface, uint32_t ip, uint32_t bytes);
+int ipacct_add_client(uint8_t ipv, const void *addr);
+int ipacct_del_client(uint8_t ipv, const void *addr);
+int ipacct_update_rx(const char *iface, uint8_t ipv, const void *addr, uint32_t bytes);
+int ipacct_update_tx(const char *iface, uint8_t ipv, const void *addr, uint32_t bytes);
 int ipacct_accumulate_kernel_delta(uint64_t rx_delta, uint64_t tx_delta,
                                    uint64_t latest_rx, uint64_t latest_tx);
 void ipacct_snapshot_and_clear(uint64_t *out_kernel_rx, uint64_t *out_kernel_tx,
